3ce8a6604a0948e531bc4b676f9b9b6951b454ad
Rodion <swizi_hs@mail.ru>
2024-11-06 19:58:53 +0500

    added game logic for all figures

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react/jsx-runtime'
 import { Board } from '../models/Board'
 import CellComponent from './CellComponent'
+import { useEffect, useState } from 'react'
+import { Cell } from '../models/Cell'
 
 interface BoardProps {
   board: Board
@@ -8,12 +10,40 @@ interface BoardProps {
 }
 
 function BoardComponent({ board, setBoard }: BoardProps) {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+
+  function click(cell: Cell) {
+    if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      selectedCell.moveFigure(cell)
+      setSelectedCell(null)
+    } else setSelectedCell(cell)
+  }
+
+  useEffect(() => {
+    highlightCells()
+  }, [selectedCell])
+
+  function highlightCells() {
+    board.highlightCells(selectedCell)
+    updateBoard()
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard()
+    setBoard(newBoard)
+  }
+
   return (
     <div className="board">
       {board.cells.map((row, index) => (
         <Fragment key={index}>
           {row.map(cell => (
-            <CellComponent cell={cell} key={cell.id} />
+            <CellComponent
+              click={click}
+              cell={cell}
+              key={cell.id}
+              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+            />
           ))}
         </Fragment>
       ))}
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,10 +2,21 @@ import { Cell } from '../models/Cell'
 
 interface CellProps {
   cell: Cell
+  selected: boolean
+  click: (cell: Cell) => void
 }
 
-function CellComponent({ cell }: CellProps) {
-  return <div className={['cell', cell.color].join(' ')}>{cell.figure?.logo && <img src={cell.figure.logo} />}</div>
+function CellComponent({ cell, selected, click }: CellProps) {
+  return (
+    <div
+      className={['cell', cell.color, selected && 'selected'].join(' ')}
+      onClick={() => click(cell)}
+      style={{ background: cell.available && cell.figure ? 'green' : '' }}
+    >
+      {cell.available && !cell.figure && <div className="available" />}
+      {cell.figure?.logo && <img src={cell.figure.logo} />}
+    </div>
+  )
 }
 
 export default CellComponent
